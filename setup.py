af60c1f623f43e57c60be76d2aafcc8c177c5d7f
Tom Manley <tom.manley@gmail.com>
2012-06-13 00:25:19 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='MaxSonar',
+    version='0.1dev',
+    packages=['maxsonar',],
+    license='BSD',
+    long_description=open('README.rst').read(),
+)
